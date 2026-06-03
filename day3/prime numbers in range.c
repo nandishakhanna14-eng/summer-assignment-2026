b266efd91a07a95a4dd06cdc363965b9a3c0b90e
nandishakhanna14-eng <nandishakhanna14@gmail.com>
2026-06-03 10:39:58 +0530

    day3(gcd.c-prime.c)

---
+++ day3/prime numbers in range.c
@@ -0,0 +1,27 @@
+#inlude <stdio.h>
+intmain()
+{
+    int start ,end,i,j, isprime;
+    printf("Enter the starting number: ");
+    scanf("%d",&start);
+    printf("Enter the ending number: ");
+    scanf("%d",&end);
+
+    printf("Prime numbers between %d and %d are: ", start, end);
+    for(i=start; i<=end; i++)
+    {
+        if(i<2)
+            continue;
+        isprime=1;
+        for(j=2; j<=i/2; j++)
+        {
+            if(i%j==0)
+            {
+                isprime=0;
+                break;
+            }
+        }
+        if(isprime)
+            printf("%d ", i);
+    }
+}
